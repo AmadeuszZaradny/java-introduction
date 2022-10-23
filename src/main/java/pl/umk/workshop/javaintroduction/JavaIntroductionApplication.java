e5e42c6a51a77e9510e3a45625e05677cc5475b2
Amadeusz <amadeusz.zaradny@allegro.pl>
2022-10-23 15:34:08 +0200

    Base

---
+++ src/main/java/pl/umk/workshop/javaintroduction/JavaIntroductionApplication.java
@@ -0,0 +1,13 @@
+package pl.umk.workshop.javaintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/umk/workshop/javaintroduction/JavaIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umk.workshop.javaintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
