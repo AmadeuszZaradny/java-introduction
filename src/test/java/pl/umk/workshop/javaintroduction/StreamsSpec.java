45add03f227718354eb88e90305de516532ca0d6
Amadeusz <amadeusz.zaradny@allegro.pl>
2023-10-24 21:34:39 +0200

    first 10 prime numbers instead of 8

--- src/test/java/pl/umk/workshop/javaintroduction/StreamsSpec.java
+++ src/test/java/pl/umk/workshop/javaintroduction/StreamsSpec.java
@@ -141,6 +141,6 @@ public class StreamsSpec {
         var primeNumbers = emptyList();
 
         //then
-        assertEquals(asList(2, 3, 5, 7, 11, 13, 17, 19), primeNumbers);
+        assertEquals(asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29), primeNumbers);
     }
 }
