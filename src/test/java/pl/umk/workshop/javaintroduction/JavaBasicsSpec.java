88dd393761f7d34446957822e60d3e0f434b8586
Amadeusz <amadeusz.zaradny@allegro.pl>
2023-11-14 21:01:44 +0100

    Added comments

--- src/test/java/pl/umk/workshop/javaintroduction/JavaBasicsSpec.java
+++ src/test/java/pl/umk/workshop/javaintroduction/JavaBasicsSpec.java
@@ -13,13 +13,17 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// Zadania są od siebie zależne. Należy wykonywac je w kolejnosci.
+// Zadania są od siebie zależne. Należy wykonywać je w kolejnosci.
 @SpringBootTest
 public class JavaBasicsSpec {
 
     @Test
         // Zmien klasę MutableUser tak aby test przechodził
         // Tip: HashMap korzysta z metody hashCode oraz equals
+        // Komentarz: HashMapa to struktura, która przechowuje dane w parach klucz-wartość, ułatwiając szybkie wyszukiwanie.
+        // Metoda `hashCode` jest w niej używana do przypisania każdemu kluczowi unikalnego kodu, który służy do jego umiejscowienia w strukturze.
+        // Metoda `equals` pozwala sprawdzić, czy dwa klucze są takie same.
+        // Ważne jest to, aby upewnić się, że klucz jest unikalny, bo inaczej może to powodować wycieki pamięci jak w zadaniu.
     void equalsAndHashcode() {
         //given
         HashMap<MutableUser, Integer> userMap = new HashMap<>();
@@ -36,6 +40,8 @@ public class JavaBasicsSpec {
     @Test
         // Zmien klasę MutableUser oraz sekcję 'when' tak aby test przechodził
         // Tip: setter
+        // Komentarz: Obiekt `user` jest mutowalny co oznacza, że po jego utworzeniu możliwa jest modyfikacja pól (danych),
+        // które w nim się znajduą. O konsekwencjach takiego podejścia dowiesz się w zadaniu `mutabilityProblem`
     void mutability() {
         //given
         MutableUser user = new MutableUser("Jacek", "Kowalski");
@@ -51,6 +57,11 @@ public class JavaBasicsSpec {
     @Test
         // Zmieniając sekcje 'when' spraw aby test przeszedł
         // Tip: setter
+        // Komentarz: Ze względu na to, że obiekt `user` jest mutowalny istnieje możliwość zmiany jego zawartości podczas
+        // działania aplikacji. Jest to szczególnie niebezpieczne w wielowątkowych aplikacjach gdzie kilka operacji może dziać
+        // się równolegle. Wyobraź sobię sytuację w której taki obiekt przechowuje informację o ilości pieniędzy, które należy
+        // pobrać z konta pewnemu użytkownikowi, wtedy zamiast podmiany nazwiska jak w zadaniu możemy mieć do czynienia z dużymi
+        // stratami finansowymi.
     void mutabilityProblem() {
         //given
         MutableUser user = new MutableUser("Jacek", "Kowalski");
@@ -66,6 +77,9 @@ public class JavaBasicsSpec {
     @Test
         // Zmien klasę ImmutableUser tak aby test przechodził
         // Tip: final keyword
+        // Komentarz: Po ustawieniu pól klasy na `final` mamy pewność, że dane, które w nim się znajduą nie będą
+        // zmienione podczas działania aplikacji. Taki obiekt może być dostępny dla wielu wątków
+        // bez konsekwencji opisanych w zadaniu `mutabilityProblem`.
     void immutability() {
         //given
         ImmutableUser user = new ImmutableUser("Jacek", "Kowalski");
@@ -81,7 +95,8 @@ public class JavaBasicsSpec {
     @Test
         // Zmien sekcję 'when' tak aby test przechodził
         // Pamiętaj, że pola klasy są już niemutowalne
-        // Top: operator new
+        // Tip: operator new
+        // Komentarz: Aby "edytować" obiekt musimy uworzyć go od nowa ze zmienionymi danymi.
     void editImmutableField() {
         //given
         ImmutableUser user = new ImmutableUser("Jacek", "Kowalski");
@@ -99,6 +114,9 @@ public class JavaBasicsSpec {
         // Zmien klasy Client, Seller i Admin tak aby test przechodził
         // Nie definiuj pól 'name' i 'surname' bezpośrednio w tych klasach
         // Tip: Zerknij do klas PlatformUser i  PlatformFacade
+        // Komentarz: Interfejs `PlatformUser` pozwala nam na zdefiniowanie swego rodzaju kontraktu, który musi zostać
+        // spełniony przez każdą klasę, która go implementuje. Dzięki temu możemy tworzyć użytkowników, którzy posiadają
+        // różne zbiory praw.
     void inheritanceAndPolymorphism() {
         //given
 
